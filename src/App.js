f1f2309df979a01057b419e563459c4116d067e1
carlos <carlosjuniorrvega@hotmail.com>
2023-02-02 13:39:22 -0500

    feat: initial layout and styling. CV

--- src/App.js
+++ src/App.js
@@ -12,15 +12,15 @@ function App() {
         </div>
         <div className="botton">
           <div className="feels">
-            <p>65 F</p>
+            <p className="bold">65 F</p>
             <p>Feels like</p>
           </div>
           <div className="humidity">
-            <p>20 %</p>
+            <p className="bold">20 %</p>
             <p>humidity</p>
           </div>
-          <div className="speed">
-            <p>12 MPH</p>
+          <div className="wind">
+            <p className="bold">12 MPH</p>
             <p>Wind speed</p>
           </div>
         </div>
