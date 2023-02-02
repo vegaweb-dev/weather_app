1b608e61ba5a9f4906519228adc8a129f1351856
carlos <carlosjuniorrvega@hotmail.com>
2023-02-02 16:01:06 -0500

    feat: test data is fetched on hitting Enter on keyboard. CV

--- src/App.js
+++ src/App.js
@@ -1,6 +1,32 @@
+import { useState } from 'react';
+import axios from 'axios';
+
 function App() {
+  const [data, setData] = useState({});
+  const [location, setLocation] = useState('');
+
+  const url =
+    'https://api.openweathermap.org/data/2.5/weather?q=Dallas&appid=82a8404a413a55f0e86b63db9115879a';
+
+  const searchLocation = () => {
+    if (event.key === 'Enter') {
+      axios.get(url).then((response) => {
+        setData(response);
+        console.log(response);
+      });
+    }
+  };
+
   return (
     <div className="app">
+      <div className="search">
+        <input
+          type="text"
+          value={location}
+          onChange={(event) => setLocation(event.target.value)}
+          onKeyPress={searchLocation}
+        />
+      </div>
       <div className="container">
         <div className="top">
           <div className="city">
